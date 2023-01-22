954e2d8acc81bf517dc6f1e1b176e8110338a188
Thomas Holmes <thomasholmes@outlook.com>
2023-01-22 21:18:57 +0000

    Started navigation.

--- twentytwo/TwentyTwo.cs
+++ twentytwo/TwentyTwo.cs
@@ -82,4 +82,59 @@ public class TwentyTwo
         return new PuzzleInput(map, instructions.ToArray());
 
     }
+
+    public async Task Navigate(string filePath)
+    {
+
+        var puzzleInput = await LoadFlatMapFromFilePath(filePath);
+
+        var flatMap = puzzleInput.FlatMap;
+        var graph = CubeMap.CreateUnconnected(flatMap);
+        graph.ConnectEdges();
+
+        // start location is top left face, which is always the first
+        int x = graph.Faces.First().X;
+        int y = graph.Faces.First().Y;
+
+        int direction = CubeConsts.EAST_INDEX;
+        var heading = CalcHeading(direction);
+        var face = graph.Faces.First();
+
+        foreach (var instruction in puzzleInput.Instructions)
+        {
+
+            foreach (var _ in Enumerable.Range(0, instruction.TilesToMove))
+            {
+
+                int newX = x + heading.x;
+                int newY = y + heading.y;
+
+                if (face != graph.FetchFaceAtLocation(newX, newY))
+                {
+                    // just moved face. hold onto your butts
+                    // FetchFaceAtLocation is not reliable until coords have been resolved
+
+                    var connection = face.Connections[direction];
+
+
+                }
+
+                flatMap.FetchTile(newX, newY);
+            }
+        }
+    }
+
+    private Point CalcHeading(int direction)
+    {
+
+        switch (direction % 4)
+        {
+            case 0: return new Point(0, -1);
+            case 1: return new Point(1, 0);
+            case 2: return new Point(0, 1);
+            case 3: return new Point(-1, 0);
+            default: throw new Exception($"Unknown direction: {direction}.");
+        }
+    }
+
 }
